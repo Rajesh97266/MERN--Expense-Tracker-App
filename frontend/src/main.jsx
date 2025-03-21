edf3baf5490fb59d7ff605e6c545e24f4a06f139
Rajesh97266 <rajesh97266@gmail.com>
2025-03-21 11:51:24 +0530

    react-query-added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,21 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+        <ToastContainer position="top-right" autoClose={3000} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
