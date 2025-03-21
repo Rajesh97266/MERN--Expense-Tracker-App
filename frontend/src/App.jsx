cbe09721985ad70d69006c216bc09b0adfbc38a5
Rajesh97266 <rajesh97266@gmail.com>
2025-03-21 16:20:30 +0530

    user-login-completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HeroSection from "./components/Home/HomePage";
 import PublicNavbar from "./components/Navbar/PublicNavbar";
+import PrivateNavbar from "./components/Navbar/PrivateNavbar";
 import LoginForm from "./components/Users/Login";
 import RegistrationForm from "./components/Users/Register";
 
+import { useSelector } from "react-redux";
+
 function App() {
+
+  const user = useSelector((state) => state?.auth?.user);
+  console.log(user);
   
+ 
   return (
     <BrowserRouter>
       {/* Navbar */}
-      <PublicNavbar />
+      {user ? <PrivateNavbar /> : <PublicNavbar />}
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/login" element={<LoginForm />} />
