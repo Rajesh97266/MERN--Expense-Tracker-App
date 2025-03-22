10ed237dcb660062d1c0a75d1b26903f56d1e561
Rajesh97266 <rajesh97266@gmail.com>
2025-03-22 12:30:16 +0530

    category-services

--- backend/controllers/categoryCtrl.js
+++ backend/controllers/categoryCtrl.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Category = require("../models/Category");
+
 const categoryController = {
   //!add
   create: asyncHandler(async (req, res) => {
@@ -9,12 +10,12 @@ const categoryController = {
     }
     //Convert the name to lowercase
     const normalizedName = name.toLowerCase();
-    //* Check if the type is valid
+    //! Check if the type is valid
     const validTypes = ["income", "expense"];
     if (!validTypes.includes(type.toLowerCase())) {
       throw new Error("Invalid category type" + type);
     }
-    //*Check if category already exists on the user
+    //!Check if category already exists on the user
     const categoryExists = await Category.findOne({
       name: normalizedName,
       user: req.user,
@@ -24,7 +25,7 @@ const categoryController = {
         `Category ${categoryExists.name} already exists in the database`
       );
     }
-    //*Create the category
+    //! Create the category
     const category = await Category.create({
       name: normalizedName,
       user: req.user,
@@ -33,19 +34,55 @@ const categoryController = {
     res.status(201).json(category);
   }),
 
-  // !lists
+  //!lists
   lists: asyncHandler(async (req, res) => {
     const categories = await Category.find({ user: req.user });
     res.status(200).json(categories);
   }),
 
-  // !Update
+  //!update
   update: asyncHandler(async (req, res) => {
-    const { name, type } = req.body;
+    const { categoryId } = req.params;
+    const { type, name } = req.body;
+    const normalizedName = name.toLowerCase();
+    const category = await Category.findById(categoryId);
+    if (!category && category.user.toString() !== req.user.toString()) {
+      throw new Error("Category not found or user not authorized");
+    }
+    const oldName = category.name;
+    //! Update category properties
+    category.name = normalizedName || category.name;
+    category.type = type || category.type;
+    const updatedCategory = await category.save();
+    //Update affected transaction
+    if (oldName !== updatedCategory.name) {
+      await Transaction.updateMany(
+        {
+          user: req.user,
+          category: oldName,
+        },
+        { $set: { category: updatedCategory.name } }
+      );
+    }
+    res.json(updatedCategory);
   }),
-
-  //!Delete
-  delete: asyncHandler(async (req, res) => {}),
+  //! delete
+  delete: asyncHandler(async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if (category && category.user.toString() === req.user.toString()) {
+      //!  Update transactions that have this category
+      const defaultCategory = "Uncategorized";
+      await Transaction.updateMany(
+        { user: req.user, category: category.name },
+        { $set: { category: defaultCategory } }
+      );
+      //! Remove category
+      await Category.findByIdAndDelete(req.params.id);
+      res.json({ message: "Category removed and transactions updated" });
+    } else {
+      res.json({ message: "Category not found or user not authorized" });
+    }
+  }),
 };
 
 module.exports = categoryController;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,15 @@ import PublicNavbar from "./components/Navbar/PublicNavbar";
 import PrivateNavbar from "./components/Navbar/PrivateNavbar";
 import LoginForm from "./components/Users/Login";
 import RegistrationForm from "./components/Users/Register";
-
+import AddCategory from "./components/Category/AddCategory";
 import { useSelector } from "react-redux";
+import CategoriesList from "./components/Category/CategoriesList";
+import UpdateCategory from "./components/Category/UpdateCategory";
 
 function App() {
-
   const user = useSelector((state) => state?.auth?.user);
   console.log(user);
-  
- 
+
   return (
     <BrowserRouter>
       {/* Navbar */}
@@ -21,6 +21,9 @@ function App() {
         <Route path="/" element={<HeroSection />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/add-category" element={<AddCategory />} />
+        <Route path="/categories" element={<CategoriesList />} />
+        <Route path="/update-category/:id" element={<UpdateCategory />} />
       </Routes>
     </BrowserRouter>
   );
